b76a1ceb2d8922d1abafad900a80460d2f938b0a
Alex Rodin <bomvendador@yandex.ru>
2017-01-31 14:25:30 +0300

    Merge remote-tracking branch 'refs/remotes/origin/master'
    # Conflicts:
    #	.idea/dictionaries/_.xml
    #	.idea/workspace.xml
    #	dashboard_ru/templates/admin_details.html
    #	ru/templates/index.html
    #	static/css/testimonials-carousel/style.css
    #	trans

--- ru/views.py
+++ ru/views.py
@@ -205,6 +205,7 @@ def user_client_add(name, email, tel):
 
 
 def save_files_trans(request):
+    print('ddd');
     if request.method == 'POST':
         data = request.POST
         user_exists = False
