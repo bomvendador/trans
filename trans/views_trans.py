c696a3893476d42d63b64ea74d98d24f2423b969
Alex Rodin <bomvendador@yandex.ru>
2017-02-02 18:20:56 +0300

    Merge remote-tracking branch 'refs/remotes/origin/master'
    # Conflicts:
    #	.idea/dictionaries/_.xml
    #	.idea/workspace.xml
    #	dashboard_ru/templates/admin_details.html
    #	ru/templates/index.html
    #	static/css/testimonials-carousel/style.css
    #	trans

--- trans/views_trans.py
+++ trans/views_trans.py
@@ -2,12 +2,17 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.messages import get_messages
+from django.contrib import messages
 
 
 @csrf_exempt
 def payment_success(request):
     if request.method == 'POST':
+
+        messages.add_message(request, messages.INFO, 'payment')
+
         context = {
-            'data': request.POST['LMI_PAYMENT_AMOUNT']
+            'data': request.POST
         }
     return render(request, 'success.html', context)
