e2d0836507496ea5dfe883edd0b711200e5c8439
Alex Rodin <bomvendador@yandex.ru>
2017-02-10 14:07:41 +0300

    Merge remote-tracking branch 'refs/remotes/origin/master'
    # Conflicts:
    #	.idea/dictionaries/_.xml
    #	.idea/workspace.xml
    #	dashboard_ru/templates/admin_details.html
    #	ru/templates/index.html
    #	static/css/testimonials-carousel/style.css
    #	trans

--- dashboard_ru/views.py
+++ dashboard_ru/views.py
@@ -808,6 +808,7 @@ def delete_translation_file_from_order(request):
         file_id = json_data['file_id']
         order = SentDoc.objects.get(id=TranslationFiles.objects.get(id=file_id).order.id)
         order_files_qnt = TranslationFiles.objects.filter(order=order).count()
+        logger.debug('file qnt = ' + str(order_files_qnt))
         order.translation_downloaded = False
         if order_files_qnt == 0:
             order.translation_files = False
