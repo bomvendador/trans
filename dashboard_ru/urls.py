e539cabb7abd6cd4c81d7982352dc7bebc115a5a
Alex Rodin <bomvendador@yandex.ru>
2017-02-13 15:01:52 +0300

    Merge remote-tracking branch 'refs/remotes/origin/master'
    # Conflicts:
    #	.idea/dictionaries/_.xml
    #	.idea/workspace.xml
    #	dashboard_ru/templates/admin_details.html
    #	ru/templates/index.html
    #	static/css/testimonials-carousel/style.css
    #	trans

--- dashboard_ru/urls.py
+++ dashboard_ru/urls.py
@@ -78,6 +78,7 @@ urlpatterns = [
     # url(r'^reg/$', views.reg_user, name='reg_user'),
 
     url(r'^testimonials_list/$', ru_views.get_testimonials_list, name='testimonials_list'),
+    url(r'^save_testimonial/$', views.save_testimonial, name='save_testimonial'),
     url(r'^testimonial/(?P<testimonial_id>\d+)/$', views.testimonial, name='testimonial'),
 
 ]
