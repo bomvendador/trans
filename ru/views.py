da2532bcd9958262ed19c52f4d863f1b5667fe5f
Alex Rodin <bomvendador@yandex.ru>
2017-02-16 18:21:09 +0300

    Merge remote-tracking branch 'refs/remotes/origin/master'
    # Conflicts:
    #	.idea/dictionaries/_.xml
    #	.idea/workspace.xml
    #	dashboard_ru/templates/admin_details.html
    #	ru/templates/index.html
    #	static/css/testimonials-carousel/style.css
    #	trans

--- ru/views.py
+++ ru/views.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+# from __future__ import unicode_literals
 import sys
 from uuid import uuid4
 import unicodedata
@@ -572,15 +572,15 @@ def get_new_testimonials_list(request):
     return render(request, 'testimonials_list.html', context)
 
 
-def path_and_rename(path):
-    def wrapper(instance, filename):
-        ext = filename.split('.')[-1]
-        # get filename
-        if instance.pk:
-            filename = '{}.{}'.format(instance.pk, ext)
-        else:
-            # set filename as random string
-            filename = '{}.{}'.format(uuid4().hex, ext)
-        # return the whole path to the file
-        return os.path.join(path, filename)
-    return wrapper
+# def path_and_rename(path):
+#     def wrapper(instance, filename):
+#         ext = filename.split('.')[-1]
+#         # get filename
+#         if instance.pk:
+#             filename = '{}.{}'.format(instance.pk, ext)
+#         else:
+#             # set filename as random string
+#             filename = '{}.{}'.format(uuid4().hex, ext)
+#         # return the whole path to the file
+#         return os.path.join(path, filename)
+#     return wrapper
