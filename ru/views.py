fa07812296bd17a4c2cbaab07388a7a299a3e78c
Alex Rodin <bomvendador@yandex.ru>
2017-02-18 00:46:52 +0300

    Merge remote-tracking branch 'refs/remotes/origin/master'
    # Conflicts:
    #	.idea/dictionaries/_.xml
    #	.idea/workspace.xml
    #	dashboard_ru/templates/admin_details.html
    #	ru/templates/index.html
    #	static/css/testimonials-carousel/style.css
    #	trans

--- ru/views.py
+++ ru/views.py
@@ -499,6 +499,7 @@ def save_files_trans(request):
             doc_sent.save()
             # sys.getdefaultencoding = lambda : 'UTF-8'
             for f in request.FILES.getlist('file'):
+                logger.debug('file = ' + str(f.name))
                 file_name = f.name.split('.')
                 # s = SentFiles(file=f, sent_doc=doc_sent, file_name=f.name)
                 s = SentFiles(file=f, sent_doc=doc_sent)
