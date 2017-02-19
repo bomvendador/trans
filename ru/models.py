5bf78c2e154138b48961df8a7164534302d8fe6c
Alex Rodin <bomvendador@yandex.ru>
2017-02-19 23:10:57 +0300

    Merge remote-tracking branch 'refs/remotes/origin/master'
    # Conflicts:
    #	.idea/dictionaries/_.xml
    #	.idea/workspace.xml
    #	dashboard_ru/templates/admin_details.html
    #	ru/templates/index.html
    #	static/css/testimonials-carousel/style.css
    #	trans

--- ru/models.py
+++ ru/models.py
@@ -294,7 +294,6 @@ class SentFiles (models.Model):
     sent_doc = models.ForeignKey(SentDoc)
     added = models.DateTimeField(auto_now_add=True, auto_now=False)
     changed = models.DateTimeField(auto_now=True, auto_now_add=False, null=True)
-    added_by = models.ForeignKey(User, null=True, blank=True)
 
     def filename(self):
         return os.path.basename(self.file.name)
