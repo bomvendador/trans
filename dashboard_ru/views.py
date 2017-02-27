de42e3932f4d2414539ceae8a2e744b184d8db68
Alex Rodin <bomvendador@yandex.ru>
2017-02-27 14:21:46 +0300

    Merge remote-tracking branch 'refs/remotes/origin/master'
    # Conflicts:
    #	.idea/dictionaries/_.xml
    #	.idea/workspace.xml
    #	dashboard_ru/templates/admin_details.html
    #	ru/templates/index.html
    #	static/css/testimonials-carousel/style.css
    #	trans

--- dashboard_ru/views.py
+++ dashboard_ru/views.py
@@ -1087,6 +1087,7 @@ def update_order_payment(request):
         order_id = request.POST.get('order_id')
         sent_doc = SentDoc.objects.get(id=order_id)
         client = Client.objects.get(user=sent_doc.user)
+        payment = Payment()
         if sent_doc.status.name != u'Выполнен':
             sent_doc.status = OrderStatus.objects.get(name=u'В работе')
         payment_amount = request.POST.get('paid_amount')
@@ -1101,15 +1102,17 @@ def update_order_payment(request):
         if not ',' in payment_date:
             parsed_datetime = parse_date_as_datetime(payment_date)
             sent_doc.payment_date = parse_datetime(parsed_datetime)
+            payment.date = parse_datetime(parsed_datetime)
 
-        sent_doc.payment_amount = payment_amount
+        payment.amount = payment_amount
+        payment.method = PayMethod.objects.get(name=payment_method)
         # logger.debug('date = ' + str(parsed_datetime))
 
         # print(str(payment_date) + str(payment_method) + str(payment_amount))
-        sent_doc.paymethod = PayMethod.objects.get(name=payment_method)
         # logger.debug('method = ' + str(payment_method).decode('utf-8'))
         update_client_statistics(sent_doc.user)
         sent_doc.save()
+        payment.save()
         return HttpResponse('')
 
 
