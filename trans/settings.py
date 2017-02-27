e1c2fa640250d712a49ed44674b0f771ffedd1ea
Alex Rodin <bomvendador@yandex.ru>
2017-02-27 21:55:08 +0300

    Merge remote-tracking branch 'refs/remotes/origin/master'
    # Conflicts:
    #	.idea/dictionaries/_.xml
    #	.idea/workspace.xml
    #	dashboard_ru/templates/admin_details.html
    #	ru/templates/index.html
    #	static/css/testimonials-carousel/style.css
    #	trans

--- trans/settings.py
+++ trans/settings.py
@@ -16,7 +16,7 @@ import os
 
 # SESSION_ENGINE = 'redis_sessions.session'
 
-PROLINGVA_TEL = '+7 800-000-00-00'
+PROLINGVA_TEL = '+7 800-100-28-60'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
