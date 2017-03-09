bdf784490c0cb23363227cc086f5403603b6018b
Alex Rodin <bomvendador@yandex.ru>
2017-03-10 01:07:30 +0300

    Merge remote-tracking branch 'refs/remotes/origin/master'
    # Conflicts:
    #	.idea/dictionaries/_.xml
    #	.idea/workspace.xml
    #	dashboard_ru/templates/admin_details.html
    #	ru/templates/index.html
    #	static/css/testimonials-carousel/style.css
    #	trans

--- trans/settings.py
+++ trans/settings.py
@@ -21,7 +21,7 @@ PROLINGVA_TEL = '+7 800-100-28-60'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECURE_SSL_REDIRECT = True
+# SECURE_SSL_REDIRECT = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
